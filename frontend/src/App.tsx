656521b7184ba273269c8dc04af17d3902d3910b
MSI\nazri <nazrinamirrullah@gmail.com>
2025-07-08 23:12:14 +0800

    feat:Import Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card border-2 border-red-500 flex flex-col items-center justify-center gap-10">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
