264e643ea90f1b226b32386daf3c6155d47f8160
MSI\nazri <nazrinamirrullah@gmail.com>
2025-09-19 17:59:58 +0800

    feat: Add fields to Meeting and Waitlist tables

--- backend/database/migrations/2025_09_19_084246_create_waitlist_table.php
+++ backend/database/migrations/2025_09_19_084246_create_waitlist_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('waitlist', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('business');
             $table->timestamps();
         });
     }
--- backend/database/migrations/2025_09_19_084324_create_meeting_table.php
+++ backend/database/migrations/2025_09_19_084324_create_meeting_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('meeting', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('business');
             $table->timestamps();
         });
     }
