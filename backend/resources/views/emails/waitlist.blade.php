f670da4225143dcb0137915cf77200f480ccb76d
MSI\nazri <nazrinamirrullah@gmail.com>
2025-09-30 12:23:30 +0800

    fix: Waitlist Blade images fix

--- backend/resources/views/emails/waitlist.blade.php
+++ backend/resources/views/emails/waitlist.blade.php
@@ -22,7 +22,7 @@
           <!-- 1) Logo (40 top, 40 bottom) -->
           <tr>
             <td align="center" style="padding:40px 24px 40px 24px;">
-              <img src="{{ url('storage/Group.png') }}" width="120" alt="Beres"
+              <img src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/c0ad386f-b82e-49bd-838f-fce8d92fae15/158x40.png" width="120" alt="Beres"
                    style="display:block;max-width:120px;border:0;">
             </td>
           </tr>
@@ -30,7 +30,7 @@
           <!-- 2) Hero image (flush) -->
           <tr>
             <td align="center" style="padding:0;">
-              <img src="{{ url('storage/Maskgroup.png') }}" alt="Group"
+              <img src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/84e7a66c-a427-4412-b273-e05cc9722659/570x280.png" alt="Group"
                    style="display:block;width:100%;height:auto;border:0;">
             </td>
           </tr>
@@ -69,23 +69,27 @@
               <table role="presentation" cellpadding="0" cellspacing="0" border="0">
                 <tr>
                   <td style="padding:0 12px;">
+                    <!-- Facebook -->
                     <a href="https://www.facebook.com/profile.php?id=61581070502587"><img
-                      src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/facebook.svg"
+                      src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/bc52ee33-2f03-4ec9-8176-863d89f9d101/26x26.png"
                       width="24" alt="Facebook" style="display:block;filter:invert(87%);"></a>
                   </td>
                   <td style="padding:0 12px;">
+                    <!-- X -->
                     <a href="https://x.com/"><img
-                      src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/x.svg"
+                      src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/2fc0f2e1-2011-4fba-920f-09dc384699d5/26x26.png"
                       width="24" alt="X" style="display:block;filter:invert(87%);"></a>
                   </td>
                   <td style="padding:0 12px;">
+                    <!-- Instagram -->
                     <a href="https://www.instagram.com/beresmy/"><img
-                      src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/instagram.svg"
+                      src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/8114bd93-7338-4444-9ba3-354a7a503971/26x26.png"
                       width="24" alt="Instagram" style="display:block;filter:invert(87%);"></a>
                   </td>
                   <td style="padding:0 12px;">
+                    <!-- LinkedIn -->
                     <a href="https://www.linkedin.com/company/beres-my/about/"><img
-                      src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg"
+                      src="http://cdn.mcauto-images-production.sendgrid.net/3e487ea7fce08e48/0a401e0d-f03d-4ca5-9f60-c5ea16349f22/26x26.png"
                       width="24" alt="LinkedIn" style="display:block;filter:invert(87%);"></a>
                   </td>
                 </tr>
@@ -96,9 +100,9 @@
           <!-- 7) Footer (40 above social, 40 bottom padding to card edge) -->
           <tr>
             <td align="center" style="padding:40px 20px 40px 20px;">
-              <p style="margin:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:18px;color:#8fb3a1;">
+              <p style="margin:0; font-family:Arial,Helvetica,sans-serif; font-size:12px; line-height:18px; color:#8fb3a1 !important;">
                 Beres.com.my
-              </p>
+              </p>  
             </td>
           </tr>
 
