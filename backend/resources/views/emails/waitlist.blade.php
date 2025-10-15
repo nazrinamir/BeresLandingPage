2429a291234714fd9769e55e311b7c54ebe8d426
yasminong <yasminwong00@gmail.com>
2025-10-15 12:11:32 +0800

    Fix padding

--- backend/resources/views/emails/waitlist.blade.php
+++ backend/resources/views/emails/waitlist.blade.php
@@ -58,7 +58,7 @@
 
           <!-- Social icons -->
           <tr>
-            <td align="center" style="padding:24px 0 20px 0;">
+            <td align="center" style="padding:24px 0 40px 0;">
               <table role="presentation" cellpadding="0" cellspacing="0" border="0">
                 <tr>
                   <!-- Facebook -->
