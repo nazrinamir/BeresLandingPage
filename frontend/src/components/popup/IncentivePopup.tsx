134987afc1bd622184543b4f8873fc5fbb965143
yasminong <yasminwong00@gmail.com>
2025-10-23 17:23:20 +0800

    Fix final ui

--- frontend/src/components/popup/IncentivePopup.tsx
+++ frontend/src/components/popup/IncentivePopup.tsx
@@ -22,7 +22,7 @@ const IncentivePopup = ({ onClose }: { onClose: () => void }) => (
 
       {/* Popup image Mobile*/}
       <img
-        src="/IncentiveMobile.svg"
+        src="/survey.svg"
         alt="Special Incentive"
         className="relative w-[90vw] max-w-2xl rounded-2xl z-10 block md:hidden"
       />
@@ -40,7 +40,7 @@ const IncentivePopup = ({ onClose }: { onClose: () => void }) => (
         href="https://forms.office.com/r/v27frh4uhR"
         target="_blank"
         rel="noopener noreferrer"
-        className="absolute text-xs md:text-base -bottom-4 md:-bottom-4 !bg-[#012219] !text-[#AFEB2B] font-bold px-4 py-2 md:px-8 md:py-4 rounded-full shadow-md hover:bg-lime-500 transition z-40"
+        className="absolute text-md md:text-base -bottom-4 md:-bottom-4 !bg-[#012219] !text-[#AFEB2B] font-bold px-6 py-4 md:px-8 md:py-4 rounded-full shadow-md hover:bg-lime-500 transition z-40"
       >
         <strong>Complete Survey</strong>
       </a>
